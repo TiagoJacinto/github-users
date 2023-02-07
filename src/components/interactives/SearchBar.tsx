5526204bba0ba46ac821674f71711fb3960e0b24
tiagojacintodev <tiagojacintodev@gmail.com>
2023-02-07 09:51:36 +0000

    Make search bar a controlled input

--- src/components/interactives/SearchBar.tsx
+++ src/components/interactives/SearchBar.tsx
@@ -1,24 +1,38 @@
+import { useState } from "react";
 import SearchIcon from "../../assets/icon-search.svg";
 
-interface Props {}
+interface Props {
+  changeUsername: (username: string) => void;
+  isError: boolean;
+}
 
-export const SearchBar = ({}: Props) => {
+export const SearchBar = ({ changeUsername, isError }: Props) => {
+  const [username, setUsername] = useState("");
   return (
     <form
-      className="flex w-[600px] items-center justify-between gap-6 rounded-lg py-2 pl-6 pr-2 shadow-lg dark:bg-custom-navi-blue-900"
+      className="flex w-full items-center justify-between gap-2 rounded-lg bg-white py-2 pl-2 pr-2 shadow-lg dark:bg-custom-navi-blue-900 sm:pl-6"
       onSubmit={(e) => {
         e.preventDefault();
+        changeUsername(username);
       }}
     >
-      <div className="flex flex-1 items-center gap-6">
-        <img className="h-6 w-6" src={SearchIcon} alt="search icon" />
+      <div className="flex flex-1 items-center gap-2 sm:gap-6">
+        <img
+          className="h-5 w-5 sm:h-6 sm:w-6"
+          src={SearchIcon}
+          alt="search icon"
+        />
         <input
-          className="flex-1 bg-transparent text-lg outline-none placeholder:text-custom-navi-blue-600 dark:text-white dark:placeholder:text-white"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="flex-1 bg-transparent text-sm outline-none placeholder:text-custom-navi-blue-600 dark:text-white dark:placeholder:text-white sm:text-lg"
           placeholder="Search GitHub username..."
         />
       </div>
       <div>
-        <span className="mr-6 font-bold text-red-600">No results</span>
+        {isError && (
+          <span className=" font-bold text-red-600 sm:mr-6">No results</span>
+        )}
         <button className="rounded-lg bg-custom-blue py-2.5 px-5 font-bold text-white hover:opacity-80 ">
           Search
         </button>
